285017cf642f717133ebe72a5fb8953860474303
Saurav <saurav.guptadx@gmail.com>
2021-03-20 11:15:22 +0545

    changes in app.js

--- remixclient/src/App.js
+++ remixclient/src/App.js
@@ -4,28 +4,6 @@ import { ContractABI } from "./ContractABI";
 
 import "./App.css";
 
-const web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
-web3.eth.defaultAccount = web3.eth.accounts[0];
-
-const RemixContract = new web3.eth.Contract(
-  ContractABI,
-  "0x677BB4A98566ab3e12E7178A0C06Ae3A3988A2A7"
-);
-
-function App() {
-  const [message, setMessage] = useState("");
-
-  const setData = async e => {
-    e.preventDefault();
-    const accounts = await window.ethereum.enable();
-    const account = accounts[0];
-
-    const gas = await RemixContract.methods.setMessage(message).estimateGas();
-    const result = await RemixContract.methods
-      .setMessage(message)
-      .send({ from: account, gas });
-    console.log(result);
-  };
 
   const getDefaultData = async e => {
     RemixContract.methods
